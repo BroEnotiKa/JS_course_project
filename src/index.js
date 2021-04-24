c3b4b1dfe60b179c53a13cc21bdcee4e973d8ab0
BroEnotiKa <di15425@gmail.com>
2021-04-24 10:41:56 +0500

    Speed rate + placing elements in the centre if their count < 10

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import Game from './Game.js'
 
 const cardsCount = Math.min(60, parseInt((prompt('Insert number of cards (max 60): ', '40'))));
 
+// if (cardsCount < 10) {
+//     document.querySelector('.cards_holder').style.gridTemplateColumns = `repeat(${Math.max(0, cardsCount)}, 1fr)`
+// }
+
 const game = new Game(cardsCount);
 game.create();
 
@@ -9,7 +13,6 @@ game.create();
 // TODO: Нормальная генерация поля (чтобы можно было растягивать адекватно и под мобилки заходило)
 // TODO: Сделать таймер и подсчет очков игрока
 // TODO: Сделать нормальную проверку при генерации поля (может зависнуть на бесконечность, т.к. карт может не хватить для всего поля)
-// TODO: Отрефакторить весь этот ужас и сделать его читабельнее
 
 const fs = require('fs');
 
